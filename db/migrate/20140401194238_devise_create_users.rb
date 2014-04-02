8a961378e62c059ba5a1e3d7e2bc45ed7b4dff5b
Doug Grainger <dhgrainger@gmail.com>
2014-04-02 14:16:49 -0400

    remove redundant unique true

--- db/migrate/20140401194238_devise_create_users.rb
+++ db/migrate/20140401194238_devise_create_users.rb
@@ -5,7 +5,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.boolean :admin, null: false, default: false
-      t.string :user_name, null: false, unique: true
+      t.string :user_name, null: false
 
       ## Recoverable
       t.string   :reset_password_token

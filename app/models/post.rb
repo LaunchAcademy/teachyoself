5fb4a153e010c50c840b66ea84255c28db23952f
Lydia Ho <lyd.w.ho@gmail.com>
2014-04-09 13:44:15 -0400

    added functionality to delete reviews when post is deleted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   validates :title, presence: true
   validates :url, presence: true
   validates :user, presence: true
